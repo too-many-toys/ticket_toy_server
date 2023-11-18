40381a8f85e2c7b93a7bff5abac0fa88c9106d17
mark <34611716+gmae199boy@users.noreply.github.com>
2023-11-18 22:05:54 +0900

    add save collection

--- src/model/my_collection.rs
+++ src/model/my_collection.rs
@@ -9,13 +9,34 @@ pub struct MyCollection {
     pub movie_id: i64,
     pub movie_title: String,
     pub genre: Option<Vec<super::movie::Genre>>,
-    pub image_url: Option<String>,
-    pub rating: Option<i64>,
+    pub image_url: String,
+    pub rating: Option<f64>,
     pub content: Option<String>,
     pub is_post: bool,
     pub me_too: u64,
-    pub created_at: DateTime,
+    pub watched_at: Option<DateTime>,
+    pub created_at: Option<DateTime>,
     pub updated_at: DateTime,
+}
+
+impl Default for MyCollection {
+    fn default() -> Self {
+        Self {
+            id: None,
+            author_id: ObjectId::new(),
+            movie_id: 0,
+            movie_title: "".to_string(),
+            genre: None,
+            image_url: "".to_string(),
+            rating: None,
+            content: None,
+            is_post: false,
+            me_too: 0,
+            watched_at: None,
+            created_at: Some(DateTime::now()),
+            updated_at: DateTime::now(),
+        }
+    }
 }
 
 pub fn collection_schema() -> Document {
@@ -77,6 +98,10 @@ pub fn collection_schema() -> Document {
                     "bsonType": "int",
                     "description": "must be a int",
                     "minimum": 0
+                },
+                "watched_at": {
+                    "bsonType": "date",
+                    "description": "must be a date"
                 },
                 "created_at": {
                     "bsonType": "date",
--- src/service/user.rs
+++ src/service/user.rs
@@ -1,16 +1,35 @@
+use std::str::FromStr;
+
+use std::path::Path;
+
 use axum::{
+    body::Bytes,
     extract::{Multipart, State},
     Extension, Json,
 };
-use mongodb::bson::doc;
+use mongodb::bson::{doc, oid::ObjectId, DateTime};
 use serde::{Deserialize, Serialize};
+use tokio::fs::write;
 
 use crate::{
     auth,
     config::UserState,
     errors::AppError,
-    model::user::{Claims, User, UserAccountType},
+    model::{
+        my_collection::MyCollection,
+        user::{Claims, User, UserAccountType},
+    },
 };
+
+pub struct ReqMyCollection {
+    movie_id: i64,
+    movie_title: String,
+    is_post: Option<bool>,
+    watched_at: Option<String>,
+    rating: Option<f64>,
+    content: Option<String>,
+    image: Bytes,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct UserId {
@@ -89,13 +108,55 @@ pub async fn put_my_collection(
     State(user_state): State<UserState>,
     mut multipart: Multipart,
 ) -> Result<String, AppError> {
-    tracing::info!("token: {:?}", token);
+    let mut my_collection = MyCollection::default();
+    my_collection.author_id = ObjectId::from_str(token.sub.clone().as_str()).unwrap();
     while let Some(mut field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
 
-        println!("Length of `{}` is {} bytes", name, data.len());
+        match name.as_str() {
+            "movie_id" => {
+                my_collection.movie_id = field.text().await.unwrap().parse().unwrap();
+            }
+            "movie_title" => {
+                my_collection.movie_title = field.text().await.unwrap();
+            }
+            // TODO: 컬렉션에서 바로 포스트로 올리는 기능 출시하면 추가
+            // "is_post" => {
+            //     let is_post = field.text().await.unwrap();
+            // }
+            "rating" => {
+                my_collection.rating = Some(field.text().await.unwrap().parse().unwrap());
+            }
+            "content" => {
+                my_collection.content = Some(field.text().await.unwrap());
+            }
+            "image" => {
+                let path = format!("/usr/local/var/images/{}", token.sub.clone());
+                let url = format!("{}.jpg", DateTime::now().to_string());
+
+                if !Path::new(&path).exists() {
+                    tokio::fs::create_dir(&path).await.unwrap();
+                }
+                let result = write(
+                    format!(
+                        "/usr/local/var/images/{}/{}",
+                        token.sub.clone(),
+                        url.clone()
+                    ),
+                    field.bytes().await.unwrap(),
+                )
+                .await;
+                if let Err(e) = result {
+                    return Err(AppError::Api(e.to_string()));
+                }
+
+                my_collection.image_url = url;
+            }
+            _ => {}
+        }
     }
+
+    tracing::info!("collection: {:?}", my_collection);
 
     Ok("asdf".to_string())
 }
